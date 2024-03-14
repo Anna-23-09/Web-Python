b84ebe21b076bbf38c182a1097e563dd1143bc82
Anna <tolstovaaniuta@yandex.ru>
2024-03-14 17:23:13 +0300

    2 sem added

--- Seminars/Seminar_2/task2/conftest.py
+++ Seminars/Seminar_2/task2/conftest.py
@@ -7,6 +7,42 @@ with open("testdata.yaml") as f:
 
 
 @pytest.fixture
+def add_post_selector():
+    return """//*[@id="create-btn"]"""
+
+
+@pytest.fixture
+def add_title():
+    return '//*[@id="create-item"]/div/div/div[1]/div/label/input'
+
+
+@pytest.fixture
+def add_description():
+    return '//*[@id="create-item"]/div/div/div[2]/div/label/span'
+
+
+@pytest.fixture
+def add_content():
+    return '//*[@id="create-item"]/div/div/div[3]/div/label'
+
+
+@pytest.fixture
+def save_post():
+    return '//*[@id="create-item"]/div/div/div[7]/div/button/div'
+
+
+@pytest.fixture
+def check_title():
+    return '//*[@id="app"]/main/div/div[3]/div[1]/a[1]/h2'
+    # return """//*[@id="app"]/main/div/div[1]/h1"""
+
+
+@pytest.fixture
+def title_name():
+    return f"{testdata['title']}"
+
+
+@pytest.fixture
 def email_input():
     return '//*[@id="login"]/div[1]/label/input'
 
