676c252e4229f3caebd0255fbd748ca99cf0a4e4
Anna <tolstovaaniuta@yandex.ru>
2024-03-20 17:55:59 +0300

    4 lection added

---
+++ Lections/Lection_4/test_1.py
@@ -0,0 +1,29 @@
+from testpage import OperationsHelper
+import yaml
+import time
+import logging
+
+with open("./testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+
+name = testdata["email"]
+passwd = testdata["password"]
+
+def test_step_1(browser):
+    logging.info("Test1 Starting")
+    testpage = OperationsHelper(browser)
+    testpage.go_to_site()
+    testpage.enter_login("test")
+    testpage.enter_pass("test")
+    testpage.click_login_button()
+    assert testpage.get_error_text() == "401"
+
+
+def test_step_2(browser):
+    logging.info("Test1 Starting")
+    testpage = OperationsHelper(browser)
+    testpage.go_to_site()
+    testpage.enter_login("test")
+    testpage.enter_pass("test")
+    testpage.click_login_button()
+    assert testpage.get_error_text() == "401"
